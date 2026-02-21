b9da393fbb2c4638fde1c276850ba3545b257b26
manish <itzmanish001@gmail.com>
2026-02-21 14:55:41 +0530

    rest api message

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
+from rest_framework.response import Response
+from rest_framework import status
 from blog.serializers import *
 
 
@@ -9,6 +10,9 @@ from blog.serializers import *
 class PostAPIView(APIView):
     serializer_class = PostSerializer
     http_method_names = ['get', 'post']
+    def get(self, request, id:int|None=None):
+        if id is None or id == 0:
+            return Response({'message': 'This is a message'}, status=status.HTTP_400_BAD_REQUEST)
 
 class LikesAPIView(APIView):
     serializer_class = LikesSerializer
@@ -17,4 +21,5 @@ class LikesAPIView(APIView):
 class CommentAPIView(APIView):
     serializers_class = CommentsSerializer
     http_method_names = ['get', 'put', 'post']
+
 
